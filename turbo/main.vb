bd5cceb31c4a62d953756e55af73fcc72fca079b
shinez <dead@hotmail.com>
2017-04-28 13:41:18 +0200

    added comments

--- turbo/main.vb
+++ turbo/main.vb
@@ -1,41 +1,41 @@
 ﻿'//         
 '//       Copyright (c) shinez 2017 - 2018
-'//       Originally made to steal of skeletons URLs, but he get idiot and then realese??!?!?
+'//       Originally made to steal of Evan's URLs, but he get idiot and then realese??!?!?
 '//         
 
 Public Class main
-    Private Sub btnStartClick(sender As Object, e As EventArgs) Handles btnStart.Click
-        attempts = 0
+    Private Sub btnStartClick(sender As Object, e As EventArgs) Handles btnStart.Click ' button to start.
+        attempts = 0 ' resets the number of attempts.
         tmTurbo.Start()
         lblStatus.Text = "[ running ]"
         txtURL.Enabled = False
     End Sub
     Dim attempts As Integer = 0
-    Private Sub turboTick(sender As Object, e As EventArgs) Handles tmTurbo.Tick
+    Private Sub turboTick(sender As Object, e As EventArgs) Handles tmTurbo.Tick ' actual turbo loop / timer.
         On Error GoTo 1
         If (wbSteam.Document.GetElementById("customURL").GetAttribute("value") = txtURL.Text) Then
-            wbSteam.Document.Forms(0).InvokeMember("submit")
+            wbSteam.Document.Forms(0).InvokeMember("submit") ' because submitting is better than clicking?
             txtAttempts.Text = attempts.ToString()
-            attempts += 1
+            attempts += 1 ' increases the attempts by 1 every tick.
         Else
-            wbSteam.Document.GetElementById("customURL").SetAttribute("value", txtURL.Text)
+            wbSteam.Document.GetElementById("customURL").SetAttribute("value", txtURL.Text) ' sets the URL bar to the desired URL.
         End If
-1:      Me.Show()
+        1:      Me.Show() ' ghetto fix.
     End Sub
-    Private Sub btnStopClick(sender As Object, e As EventArgs) Handles btnStop.Click
+    Private Sub btnStopClick(sender As Object, e As EventArgs) Handles btnStop.Click ' button to stop.
         tmTurbo.Stop()
         lblStatus.Text = "[ idle ]"
         txtURL.Enabled = True
     End Sub
-    Private Sub mainLoad(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub mainLoad(sender As Object, e As EventArgs) Handles MyBase.Load ' fixes everything on startup.
         wbSteam.Visible = False
         tmFix.Start()
     End Sub
-    Private Sub btnSignInClick(sender As Object, e As EventArgs) Handles btnSignIn.Click
+    Private Sub btnSignInClick(sender As Object, e As EventArgs) Handles btnSignIn.Click ' signs you in.
         wbSteam.Document.GetElementById("steamAccountName").SetAttribute("value", txtUsername.Text)
         wbSteam.Document.GetElementById("steamPassword").SetAttribute("value", txtPassword.Text)
         wbSteam.Document.GetElementById("SteamLogin").InvokeMember("Click")
-        If (cbBrowser.Checked = True) Then
+        If (cbBrowser.Checked = True) Then ' for mobile authenticator purposes.
             Me.Height = 510
             wbSteam.Top = 200
             wbSteam.Visible = True
@@ -45,8 +45,8 @@ Public Class main
             wbSteam.Visible = False
         End If
     End Sub
-    Private Sub wndwTransition(sender As Object, e As EventArgs) Handles tmFix.Tick
-        If (wbSteam.Document.Title = "Steam Community :: Edit Profile") Then
+    Private Sub wndwTransition(sender As Object, e As EventArgs) Handles tmFix.Tick ' fixes the window layout after signing in.
+        If (wbSteam.Document.Title = "Steam Community :: Edit Profile") Then ' ghetto way of checking if signed in.
             On Error GoTo 2
             btnSignIn.Visible = False
             txtUsername.Visible = False
@@ -60,11 +60,11 @@ Public Class main
             wbSteam.Visible = False
             tmDebug.Start()
             Me.Text = "b1g sH1n3Zz Turb0 | Account: " + txtUsername.Text
-            tmFix.Stop()
+            tmFix.Stop() ' to only apply this once.
         End If
 2:      Me.Show()
     End Sub
-    Private Sub debug(sender As Object, e As EventArgs) Handles tmDebug.Tick
+    Private Sub debug(sender As Object, e As EventArgs) Handles tmDebug.Tick ' shows a mini web browser for debugging purposes.
         On Error GoTo 3
         If (cbDebug.Checked = True) Then
             Me.Height = 390
